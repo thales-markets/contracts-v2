cc6abf203d3c5240618ff8b3fc3b347bb9d294b6
Vladan Thales <vladanpoklopic@gmail.com>
2024-03-04 13:56:18 +0100

    Update interface

--- contracts/interfaces/ISportsAMMV2Manager.sol
+++ contracts/interfaces/ISportsAMMV2Manager.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.20;
 interface ISportsAMMV2Manager {
     function isWhitelistedAddress(address _address) external view returns (bool);
 
-    function transformCollateral(uint value) external view returns (uint);
+    function transformCollateral(uint value, address collateral) external view returns (uint);
 
-    function reverseTransformCollateral(uint value) external view returns (uint);
+    function reverseTransformCollateral(uint value, address collateral) external view returns (uint);
 
     function decimals() external view returns (uint);
 }
