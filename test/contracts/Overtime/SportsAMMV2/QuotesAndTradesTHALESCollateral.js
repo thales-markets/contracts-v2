19acdd074d424102647fcf24ebbefcfca837b3d7
dgornjakovic <dgornjakovic87@gmail.com>
2024-06-02 22:35:38 +0200

    added tests for THALES collateral

--- test/contracts/Overtime/SportsAMMV2/QuotesAndTradesTHALESCollateral.js
+++ test/contracts/Overtime/SportsAMMV2/QuotesAndTradesTHALESCollateral.js
@@ -24,7 +24,10 @@ describe('SportsAMMV2 Quotes And Trades', () => {
 		collateralTHALES,
 		collateralTHALESAddress,
 		sportsAMMV2ResultManager,
-		sportsAMMV2Manager;
+		sportsAMMV2Manager,
+		mockChainlinkOracle,
+		sportsAMMV2RiskManager,
+		liveTradingProcessor;
 
 	beforeEach(async () => {
 		({
@@ -38,6 +41,9 @@ describe('SportsAMMV2 Quotes And Trades', () => {
 			collateralTHALES,
 			sportsAMMV2ResultManager,
 			sportsAMMV2Manager,
+			mockChainlinkOracle,
+			sportsAMMV2RiskManager,
+			liveTradingProcessor,
 		} = await loadFixture(deploySportsAMMV2Fixture));
 		({ firstLiquidityProvider, firstTrader } = await loadFixture(deployAccountsFixture));
 
@@ -126,6 +132,89 @@ describe('SportsAMMV2 Quotes And Trades', () => {
 					collateralTHALESAddress,
 					false
 				);
+		});
+
+		it('Should buy a live trade with THALES', async () => {
+			const quote = await sportsAMMV2.tradeQuote(
+				tradeDataCurrentRound,
+				BUY_IN_AMOUNT,
+				ZERO_ADDRESS
+			);
+
+			await sportsAMMV2RiskManager.setLiveTradingPerSportAndTypeEnabled(SPORT_ID_NBA, 0, true);
+
+			await liveTradingProcessor.connect(firstTrader).requestLiveTrade({
+				_gameId: tradeDataCurrentRound[0].gameId,
+				_sportId: tradeDataCurrentRound[0].sportId,
+				_typeId: tradeDataCurrentRound[0].typeId,
+				_line: tradeDataCurrentRound[0].line,
+				_position: tradeDataCurrentRound[0].position,
+				_buyInAmount: BUY_IN_AMOUNT,
+				_expectedQuote: quote.totalQuote,
+				_additionalSlippage: ADDITIONAL_SLIPPAGE,
+				_referrer: ZERO_ADDRESS,
+				_collateral: collateralTHALESAddress,
+			});
+
+			let requestId = await liveTradingProcessor.counterToRequestId(0);
+			console.log('requestId is ' + requestId);
+
+			await mockChainlinkOracle.fulfillLiveTrade(requestId, true, quote.totalQuote);
+
+			const activeTickets = await sportsAMMV2Manager.getActiveTickets(0, 100);
+			const ticketAddress = activeTickets[0];
+
+			const TicketContract = await ethers.getContractFactory('Ticket');
+			const userTicket = await TicketContract.attach(ticketAddress);
+
+			const marketData = await userTicket.markets(0);
+
+			expect(marketData.odd).to.equal(ethers.parseEther('0.49'));
+
+			expect(await userTicket.isLive()).to.eq(true);
+		});
+
+		it('Should buy a ticket (1 market) in THALES which gets cancelled', async () => {
+			const quoteTHALES = await sportsAMMV2.tradeQuote(
+				tradeDataCurrentRound,
+				BUY_IN_AMOUNT,
+				collateralTHALESAddress
+			);
+
+			const userBalanceBefore = await collateralTHALES.balanceOf(firstTrader);
+
+			await sportsAMMV2
+				.connect(firstTrader)
+				.trade(
+					tradeDataCurrentRound,
+					BUY_IN_AMOUNT,
+					quoteTHALES.totalQuote,
+					ADDITIONAL_SLIPPAGE,
+					ZERO_ADDRESS,
+					collateralTHALESAddress,
+					false
+				);
+
+			await sportsAMMV2ResultManager.setResultTypesPerMarketTypes([0], [RESULT_TYPE.ExactPosition]);
+
+			await sportsAMMV2ResultManager.setResultsPerMarkets(
+				[tradeDataCurrentRound[0].gameId],
+				[tradeDataCurrentRound[0].typeId],
+				[tradeDataCurrentRound[0].playerId],
+				[[-9999]]
+			);
+
+			const activeTickets = await sportsAMMV2Manager.getActiveTickets(0, 100);
+			const ticketAddress = activeTickets[0];
+
+			await sportsAMMV2.connect(firstTrader).exerciseTicket(ticketAddress);
+
+			const safeBoxTHALESBalance = await collateralTHALES.balanceOf(safeBoxTHALESAddress);
+			expect(safeBoxTHALESBalance).eq(0);
+
+			const userBalanceAfter = await collateralTHALES.balanceOf(firstTrader);
+
+			expect(userBalanceBefore).eq(userBalanceAfter);
 		});
 	});
 });
