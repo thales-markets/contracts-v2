750a1688526486db78c77fa214cdc29b5fce6c9f
KirilA <kirilantevski@gmail.com>
2024-09-11 17:55:55 +0200

    - added tests

---
+++ test/contracts/Overtime/ResolveBlocker/BlockingGames.js
@@ -0,0 +1,137 @@
+const { loadFixture } = require('@nomicfoundation/hardhat-toolbox/network-helpers');
+const { expect } = require('chai');
+const {
+	deployAccountsFixture,
+	deploySportsAMMV2Fixture,
+} = require('../../../utils/fixtures/overtimeFixtures');
+const {
+	BUY_IN_AMOUNT,
+	ADDITIONAL_SLIPPAGE,
+	SPORT_ID_NBA,
+	RESULT_TYPE,
+} = require('../../../constants/overtime');
+const { ZERO_ADDRESS } = require('../../../constants/general');
+
+describe('ResolveBlocker Blocking games', () => {
+	let sportsAMMV2,
+		sportsAMMV2Data,
+		sportsAMMV2Manager,
+		sportsAMMV2LiquidityPool,
+		resolveBlocker,
+		owner,
+		secondAccount,
+		thirdAccount,
+		tradeDataCurrentRound,
+		tradeDataTenMarketsCurrentRound,
+		firstLiquidityProvider,
+		firstTrader,
+		freeBetsHolder,
+		collateralAddress,
+		sportsAMMV2RiskManager,
+		mockChainlinkOracle,
+		liveTradingProcessor,
+		sportsAMMV2ResultManager;
+	const secondGame = '0x3361313961063935343164563637633634613865623623039435363366336666';
+	const thirdGame = '0x6106393361313935343336346138336633665623623031645636376943536666';
+
+	beforeEach(async () => {
+		({
+			sportsAMMV2,
+			sportsAMMV2Data,
+			sportsAMMV2Manager,
+			sportsAMMV2LiquidityPool,
+			resolveBlocker,
+			tradeDataCurrentRound,
+			tradeDataTenMarketsCurrentRound,
+			freeBetsHolder,
+			collateralAddress,
+			sportsAMMV2RiskManager,
+			mockChainlinkOracle,
+			liveTradingProcessor,
+			sportsAMMV2ResultManager,
+		} = await loadFixture(deploySportsAMMV2Fixture));
+		({ firstLiquidityProvider, firstTrader, owner, secondAccount, thirdAccount } =
+			await loadFixture(deployAccountsFixture));
+
+		await sportsAMMV2LiquidityPool
+			.connect(firstLiquidityProvider)
+			.deposit(ethers.parseEther('1000'));
+		await sportsAMMV2LiquidityPool.start();
+	});
+
+	describe('Game blocking', () => {
+		it('should block games when called by a whitelisted address', async () => {
+			console.log(tradeDataCurrentRound[0].gameId);
+			await resolveBlocker.connect(owner).blockGames([tradeDataCurrentRound[0].gameId, secondGame]);
+			expect(await resolveBlocker.gameIdBlockedForResolution(tradeDataCurrentRound[0].gameId)).to.be
+				.true;
+			expect(await resolveBlocker.gameIdBlockedForResolution(secondGame)).to.be.true;
+		});
+
+		it('should unblock games when called by a whitelisted address', async () => {
+			await resolveBlocker.connect(owner).blockGames([tradeDataCurrentRound[0].gameId]);
+			await resolveBlocker.connect(owner).unblockGames([tradeDataCurrentRound[0].gameId]);
+			expect(await resolveBlocker.gameIdBlockedForResolution(tradeDataCurrentRound[0].gameId)).to.be
+				.false;
+		});
+
+		it('should revert blocking games when called by a non-whitelisted address', async () => {
+			await expect(
+				resolveBlocker.connect(thirdAccount).blockGames([tradeDataCurrentRound[0].gameId])
+			).to.be.revertedWith('Invalid sender');
+		});
+
+		it('should revert unblocking games when called by a non-whitelisted address', async () => {
+			await resolveBlocker.connect(owner).blockGames([tradeDataCurrentRound[0].gameId]);
+			await expect(
+				resolveBlocker.connect(thirdAccount).unblockGames([tradeDataCurrentRound[0].gameId])
+			).to.be.revertedWith('Invalid sender');
+		});
+
+		it('should return correct blocking status with getGamesBlockedForResolution', async () => {
+			await resolveBlocker.connect(owner).blockGames([tradeDataCurrentRound[0].gameId, secondGame]);
+			const blockedGames = await resolveBlocker.getGamesBlockedForResolution([
+				tradeDataCurrentRound[0].gameId,
+				secondGame,
+				thirdGame,
+			]);
+			expect(blockedGames).to.deep.equal([true, true, false]);
+		});
+	});
+
+	describe('Set Manager and SportsAMMData', () => {
+		it('should allow the owner to set a new manager', async () => {
+			const newManager = secondAccount.address;
+			await resolveBlocker.setManager(newManager);
+			expect(await resolveBlocker.manager()).to.equal(newManager);
+		});
+
+		it('should revert when setting manager to zero address', async () => {
+			await expect(resolveBlocker.setManager(ZERO_ADDRESS)).to.be.revertedWith('Invalid address');
+		});
+
+		it('should revert when a non-owner tries to set the manager', async () => {
+			await expect(
+				resolveBlocker.connect(thirdAccount).setManager(secondAccount.address)
+			).to.be.revertedWith('Only the contract owner may perform this action');
+		});
+
+		it('should allow the owner to set a new sportsAMMData', async () => {
+			const newSportsAMMData = secondAccount.address;
+			await resolveBlocker.setSportsAMMData(newSportsAMMData);
+			expect(await resolveBlocker.sportsAMMData()).to.equal(newSportsAMMData);
+		});
+
+		it('should revert when setting sportsAMMData to zero address', async () => {
+			await expect(resolveBlocker.setSportsAMMData(ZERO_ADDRESS)).to.be.revertedWith(
+				'Invalid address'
+			);
+		});
+
+		it('should revert when a non-owner tries to set sportsAMMData', async () => {
+			await expect(
+				resolveBlocker.connect(thirdAccount).setSportsAMMData(secondAccount.address)
+			).to.be.revertedWith('Only the contract owner may perform this action');
+		});
+	});
+});
--- test/utils/fixtures/overtimeFixtures.js
+++ test/utils/fixtures/overtimeFixtures.js
@@ -720,6 +720,14 @@ async function deploySportsAMMV2Fixture() {
 
 	const safeBoxTHALESAddress = safeBoxTHALES.address;
 
+	const ResolveBlocker = await ethers.getContractFactory('ResolveBlocker');
+	const resolveBlocker = await upgrades.deployProxy(ResolveBlocker, [
+		owner.address,
+		SportsAMMV2DataAddress,
+		sportsAMMV2ManagerAddress,
+	]);
+	const resolveBlockerAddress = await resolveBlocker.getAddress();
+
 	return {
 		owner,
 		sportsAMMV2Manager,
@@ -772,6 +780,8 @@ async function deploySportsAMMV2Fixture() {
 		collateralTHALES,
 		collateralTHALESAddress,
 		sportsAMMV2THALESLiquidityPool,
+		resolveBlocker,
+		resolveBlockerAddress,
 	};
 }
 
