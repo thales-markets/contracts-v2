0906e62d2485d772d20a78ffeeb25b09b1626ee8
dgornjakovic <dgornjakovic87@gmail.com>
2024-10-25 11:39:29 +0200

    added live test

--- test/contracts/Overtime/SportsAMMV2/QuotesAndTradesTHALESCollateral.js
+++ test/contracts/Overtime/SportsAMMV2/QuotesAndTradesTHALESCollateral.js
@@ -367,6 +367,60 @@ describe('SportsAMMV2 Quotes And Trades', () => {
 			expect(await userTicket.isLive()).to.eq(true);
 		});
 
+		it('Should buy a live trade with bonus user payout', async () => {
+			const firstTraderAddress = await firstTrader.getAddress();
+			await sportsAMMV2.setAddedPayoutPercentagePerUser(firstTraderAddress, BONUS_PAYOUT);
+
+			const quote = await sportsAMMV2.tradeQuote(
+				tradeDataCurrentRound,
+				BUY_IN_AMOUNT,
+				ZERO_ADDRESS,
+				false
+			);
+
+			const quoteTHALES = await sportsAMMV2.tradeQuote(
+				tradeDataCurrentRound,
+				BUY_IN_AMOUNT,
+				collateralTHALESAddress,
+				false
+			);
+
+			await sportsAMMV2RiskManager.setLiveTradingPerSportAndTypeEnabled(SPORT_ID_NBA, 0, true);
+
+			await liveTradingProcessor.connect(firstTrader).requestLiveTrade({
+				_gameId: tradeDataCurrentRound[0].gameId,
+				_sportId: tradeDataCurrentRound[0].sportId,
+				_typeId: tradeDataCurrentRound[0].typeId,
+				_line: tradeDataCurrentRound[0].line,
+				_position: tradeDataCurrentRound[0].position,
+				_buyInAmount: BUY_IN_AMOUNT,
+				_expectedQuote: quote.totalQuote,
+				_additionalSlippage: ADDITIONAL_SLIPPAGE,
+				_referrer: ZERO_ADDRESS,
+				_collateral: collateralTHALESAddress,
+			});
+
+			let requestId = await liveTradingProcessor.counterToRequestId(0);
+			console.log('requestId is ' + requestId);
+
+			await mockChainlinkOracle.fulfillLiveTrade(requestId, true, quote.totalQuote);
+
+			const activeTickets = await sportsAMMV2Manager.getActiveTickets(0, 100);
+			const ticketAddress = activeTickets[0];
+
+			const TicketContract = await ethers.getContractFactory('Ticket');
+			const userTicket = await TicketContract.attach(ticketAddress);
+
+			const marketData = await userTicket.markets(0);
+
+			console.log('Normal quote is: ' + quote.totalQuote);
+			console.log('User live quote is: ' + marketData.odd);
+
+			expect(quoteTHALES.totalQuote).greaterThan(marketData.odd);
+
+			expect(await userTicket.isLive()).to.eq(true);
+		});
+
 		it('Should buy a ticket (1 market) in THALES which gets cancelled', async () => {
 			const quoteTHALES = await sportsAMMV2.tradeQuote(
 				tradeDataCurrentRound,
