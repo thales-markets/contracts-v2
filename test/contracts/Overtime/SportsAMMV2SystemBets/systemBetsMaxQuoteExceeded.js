7daa5265da35ca767137c8f8f93f2963049f10f2
dgornjakovic <dgornjakovic87@gmail.com>
2024-12-13 13:02:21 +0100

    allow system bets with higher quote but limit payout

--- test/contracts/Overtime/SportsAMMV2SystemBets/systemBetsMaxQuoteExceeded.js
+++ test/contracts/Overtime/SportsAMMV2SystemBets/systemBetsMaxQuoteExceeded.js
@@ -51,32 +51,42 @@ describe('SportsAMMV2 system bets', () => {
 	});
 
 	describe('Trade', () => {
-		it('Should buy a system ticket (2/3 markets)', async () => {
+		it('Should buy a system ticket (2/10 markets)', async () => {
+			await sportsAMMV2RiskManager.setTicketParams(
+				ethers.parseEther('3'),
+				15,
+				ethers.parseEther('20000'),
+				ethers.parseEther('0.5'), // 2x
+				500 // max combinations
+			);
+
 			const maxSystemBetPayoutAndQuote = await sportsAMMV2RiskManager.getMaxSystemBetPayout(
 				tradeDataTenMarketsCurrentRound,
-				5,
+				8,
 				BUY_IN_AMOUNT,
 				0
 			);
 
-			await sportsAMMV2
-				.connect(firstTrader)
-				.tradeSystemBet(
-					tradeDataTenMarketsCurrentRound,
-					BUY_IN_AMOUNT,
-					maxSystemBetPayoutAndQuote.systemBetQuote,
-					ADDITIONAL_SLIPPAGE,
-					ZERO_ADDRESS,
-					ZERO_ADDRESS,
-					false,
-					5
-				);
+			await sportsAMMV2.connect(firstTrader).tradeSystemBet(
+				tradeDataTenMarketsCurrentRound,
+				BUY_IN_AMOUNT,
+				maxSystemBetPayoutAndQuote.systemBetQuote,
+				ethers.parseEther('0.5'), // 50% additional slippage
+				ZERO_ADDRESS,
+				ZERO_ADDRESS,
+				false,
+				8
+			);
 
 			const activeTickets = await sportsAMMV2Manager.getActiveTickets(0, 100);
 			const ticketAddress = activeTickets[0];
 
 			const TicketContract = await ethers.getContractFactory('Ticket');
 			const userTicket = await TicketContract.attach(ticketAddress);
+
+			let ticketQuote = await userTicket.totalQuote();
+			console.log('ticketQuote: ' + ticketQuote.toString() / 1e18);
+
 			expect(await userTicket.isSystem()).to.be.equal(true);
 
 			expect(await userTicket.isTicketExercisable()).to.be.equal(false);
@@ -179,7 +189,12 @@ describe('SportsAMMV2 system bets', () => {
 
 			expect(await userTicket.isUserTheWinner()).to.be.equal(true);
 
+			const userBalanceBefore = await collateral.balanceOf(firstTrader);
 			await sportsAMMV2.connect(firstTrader).exerciseTicket(ticketAddress);
+			const userBalanceAfter = await collateral.balanceOf(firstTrader);
+
+			expect(userBalanceBefore).to.be.equal(ethers.parseEther('9990'));
+			expect(userBalanceAfter).to.be.equal(ethers.parseEther('10010'));
 
 			const ticketBalanceAfter = await collateral.balanceOf(ticketAddress);
 			expect(ticketBalanceAfter).to.be.equal(0);
