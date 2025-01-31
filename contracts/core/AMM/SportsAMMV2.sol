d289d54a1074c00c9f9466263c7e0342a7967c13
dgornjakovic <dgornjakovic87@gmail.com>
2025-01-31 16:34:42 +0100

    ignore free bets and staking tickets

--- contracts/core/AMM/SportsAMMV2.sol
+++ contracts/core/AMM/SportsAMMV2.sol
@@ -479,8 +479,11 @@ contract SportsAMMV2 is Initializable, ProxyOwned, ProxyPausable, ProxyReentranc
     /// @param _tickets array of tickets to be expired
     function expireTickets(address[] calldata _tickets) external onlyOwner {
         for (uint i = 0; i < _tickets.length; i++) {
-            Ticket(_tickets[i]).expire(msg.sender);
-            manager.expireKnownTicket(_tickets[i], Ticket(_tickets[i]).ticketOwner());
+            address ticketOwner = Ticket(_tickets[i]).ticketOwner();
+            if (ticketOwner != freeBetsHolder && ticketOwner != stakingThalesBettingProxy) {
+                Ticket(_tickets[i]).expire(msg.sender);
+                manager.expireKnownTicket(_tickets[i], ticketOwner);
+            }
         }
     }
 
