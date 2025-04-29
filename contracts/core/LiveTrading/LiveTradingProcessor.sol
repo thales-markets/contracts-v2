f116da703fa164f237689cbf913696da1352f19c
dgornjakovic <dgornjakovic87@gmail.com>
2025-04-29 14:00:11 +0200

    added more array indexes for correct score

--- contracts/core/LiveTrading/LiveTradingProcessor.sol
+++ contracts/core/LiveTrading/LiveTradingProcessor.sol
@@ -124,9 +124,9 @@ contract LiveTradingProcessor is ChainlinkClient, Ownable, Pausable {
         if (_allow) {
             ISportsAMMV2.TradeData[] memory tradeData = new ISportsAMMV2.TradeData[](1);
             bytes32[] memory merkleProofs;
-            uint[] memory odds = new uint[](3);
+            uint[] memory odds = new uint[](26);
             odds[lTradeData._position] = _approvedQuote;
-            ISportsAMMV2.CombinedPosition[][] memory comPositions = new ISportsAMMV2.CombinedPosition[][](3);
+            ISportsAMMV2.CombinedPosition[][] memory comPositions = new ISportsAMMV2.CombinedPosition[][](26);
 
             tradeData[0] = ISportsAMMV2.TradeData(
                 stringToBytes32(lTradeData._gameId),
